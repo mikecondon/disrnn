e2ceb905c0d7e195087bbac9daf370d842efc6ac
Michael Condon <mcondon99@gmail.com>
2025-04-09 17:43:02 +0100

    cleaned dataset reader

--- disentangled_rnns/switch_utils.py
+++ disentangled_rnns/switch_utils.py
@@ -16,63 +16,46 @@ def find(s, ch):
 def str2int(x):
   return int(x.split('_')[1])
 
-def get_dataset(addr: str, batch_size:Optional[int]=None, tr_prop: float=1, va_prop: float=0, te_prop:float=0):
+def get_dataset(df: pd.DataFrame,
+                batch_size:Optional[int]=None):
   """
-  load dataset from filename
+  Create a training, validation and test set for each mouse
   """
-  assert math.isclose(sum((tr_prop, va_prop, te_prop)), 1, rel_tol=1e-2)
-  assert tr_prop >= 0 and va_prop >= 0 and te_prop >= 0
-  df = pd.read_csv(addr)
-  mice = df.Mouse.unique()
-  ds_list = []
-  for m_i in mice:
-    # create a dataset object for this mouse
-    df_i = df[m_i == df['Mouse']]
-    df_i.loc[:, 'Session'] = df_i['Session'].map(str2int)
-    eps = df_i['Session'].value_counts().sort_index()
-    max_session_length = np.max(eps.values)
-    n_sess = len(eps)
 
-    rewards_by_session = -1 * np.ones((max_session_length, n_sess, 1))
-    choices_by_session = -1 * np.ones((max_session_length, n_sess, 1))
+  eps = df['Session'].value_counts()
+  # create a dataset object for this mouse
+  max_session_length = np.max(eps.values)
+  n_sess = len(eps)
 
-    for j, sess_j in enumerate(eps.index):
-      sess_len = eps[sess_j]
-      sess_data = df_i[df_i['Session'] == sess_j].sort_values('blockTrial')
-      rewards_by_session[0:sess_len, j, 0] = sess_data['Reward']
-      choices_by_session[0:sess_len, j, 0] = sess_data['Decision']
+  rewards_by_session = -1 * np.ones((max_session_length, n_sess, 1))
+  choices_by_session = -1 * np.ones((max_session_length, n_sess, 1))
 
-    choice_and_reward = np.concatenate(
-        (choices_by_session, rewards_by_session), axis=2
-    )
-    xs = np.concatenate(
-        (0. * np.ones((1, n_sess, 2)), choice_and_reward), axis=0
-    )
-    ys = np.concatenate((choices_by_session, -1*np.ones((1, n_sess, 1))), axis=0)
+  for j, sess_j in enumerate(eps.index):
+    # filter to just this session
+    sess_data = df[df['Session']==sess_j].sort_values('Trial')
+    sess_len = eps[sess_j]
+    rewards_by_session[0:sess_len, j, 0] = sess_data['Reward']
+    choices_by_session[0:sess_len, j, 0] = sess_data['Decision']
 
-    # permute the sessions before splitting into train, test validate datasets
-    idx = np.random.permutation(np.arange(n_sess))
-    xs = xs[:, idx, :]
-    ys = ys[:, idx, :]
-    tr_end = math.floor(tr_prop * n_sess)
-    va_end = math.floor((tr_prop + va_prop) * n_sess)
+  choice_and_reward = np.concatenate(
+      (choices_by_session, rewards_by_session), axis=2
+  )
+  xs = np.concatenate(
+      (0. * np.ones((1, n_sess, 2)), choice_and_reward), axis=0
+  )
+  ys = np.concatenate((choices_by_session, -1*np.ones((1, n_sess, 1))), axis=0)
 
-    ds_m_tr = rnn_utils.DatasetRNN(xs=xs[:, :tr_end, :], ys=ys[:, :tr_end, :], batch_size=batch_size)
-    ds_m_va = rnn_utils.DatasetRNN(xs=xs[:, tr_end:va_end, :], ys=ys[:, tr_end:va_end, :], batch_size=batch_size)
-    ds_m_te = rnn_utils.DatasetRNN(xs=xs[:, va_end:, :], ys=ys[:, va_end:, :], batch_size=batch_size)
+  ds_tr = rnn_utils.DatasetRNN(xs=xs, ys=ys, batch_size=batch_size)
 
-    ds_list.append((m_i, ds_m_tr, ds_m_va, ds_m_te))
+  return ds_tr
 
-  return ds_list
-
-def model_saver(params: dict, m_i: str, beta, dt, cv=0,loss: Optional[dict]=None):
+def model_saver(params: dict, beta, dt, cv=0,loss: Optional[dict]=None):
   directory = "/Users/michaelcondon/workspaces/pbm_group2/disentangled_rnns/models"
-  file_path = os.path.join(directory, f"params_{m_i}_{beta}_{cv}_{dt}.json")
-  file_path = os.path.join(directory, f"params_{m_i}_{beta:.0e}_{cv}_{dt}.json")
+  file_path = os.path.join(directory, f"params_{beta:.0e}_{cv}_{dt}.json")
   with open(file_path, 'w') as f:
     json.dump(params, f, indent=4, cls=rnn_utils.NpEncoder)
   if loss != None:
-    file_path = os.path.join(directory, f"loss_{m_i}_{beta:.0e}_{cv}_{dt}.json")
+    file_path = os.path.join(directory, f"loss_{beta:.0e}_{cv}_{dt}.json")
     with open(file_path, 'w') as f:
       json.dump(loss, f, indent=4, cls=rnn_utils.NpEncoder)
 
